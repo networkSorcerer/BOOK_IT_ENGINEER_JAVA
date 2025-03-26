9bc235fdb49ef2d31b9b418a06ed1fc3c8710699
networkSocerer <gigattfyhig@gmail.com>
2025-03-26 13:14:59 +0900

    정처기 자바활용 자바는 다양한 활용이 가능함

--- src/Test/Test4.java
+++ src/Test/Test4.java
@@ -1,5 +1,27 @@
 package Test;
 
+abstract class Animal {
+	String a = " is animal";
+	abstract void look();
+	void show() {
+		System.out.println("zoo");
+	}
+}
+class Chicken extends Animal {
+	Chicken(){
+		look();
+	}
+	void look() {
+		System.out.println("Chicken" + a);
+	}
+	void display() {
+		System.out.println("two wings");
+	}
+}
 public class Test4 {
+	public static void main(String [] args) {
+		Animal a = new Chicken();
+		a.show();
+	}
 
 }
