da91e162036dc34d736297fa91a5c2dba677feb8
networkSocerer <gigattfyhig@gmail.com>
2025-03-28 14:00:50 +0900

    정처기 합격 파이팅

---
+++ src/Test/Test235_26.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class Test235_26 {
+	public static void main(String [] args) {
+		int i, hap = 0;
+		for(i =1; i <=10; ++i,hap +=i);
+		System.out.printf("%d,%d\n",i,hap);
+	}
+}
---
+++ src/Test/Test237_30.java
@@ -0,0 +1,16 @@
+package Test;
+
+import java.util.Scanner;
+
+public class Test237_30 {
+	public static void main(String []args) {
+		int inNum;
+		Scanner scan01 = new Scanner(System.in);
+		inNum = scan01.nextInt();
+		if(inNum % 2== 0 )
+			System.out.printf("%d = 짝수입니다\n", inNum);
+		else 
+			System.out.printf("%d = 홀수입니다\n", inNum);
+		scan01.close();
+	}
+}
