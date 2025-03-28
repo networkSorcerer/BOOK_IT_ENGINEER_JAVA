b74c31c07a237e442bb433218a4c525971a640a1
networkSocerer <gigattfyhig@gmail.com>
2025-03-28 14:05:27 +0900

    자바

--- src/Test/Test237_30.java
+++ src/Test/Test237_30.java
@@ -7,7 +7,7 @@ public class Test237_30 {
 		int inNum;
 		Scanner scan01 = new Scanner(System.in);
 		inNum = scan01.nextInt();
-		if(inNum % 2== 0 )
+		if(inNum % 2 == 0 )
 			System.out.printf("%d = 짝수입니다\n", inNum);
 		else 
 			System.out.printf("%d = 홀수입니다\n", inNum);
