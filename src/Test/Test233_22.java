79db34c2f3ab869017e0b43b007b49fb4e278e6a
networkSocerer <gigattfyhig@gmail.com>
2025-03-28 12:49:39 +0900

    java 정저치 합격하고 취업도하자!

---
+++ src/Test/Test233_22.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class Test233_22 {
+	public static void main(String [] args) {
+		String str;
+		str = "Power overwhelming!";
+		System.out.printf("%8.4s\n", str);
+	}
+}
+
+//8자 폭의 출력 공간을 확보하고, 그 안에 최대 4자까지만 문자열을 출력하라는 뜻
